6d1b45d2b0218bc125fcd86b6e1dc908f8f3a8c2
Jonathan Phipps <jonathanmp25@gmail.com>
2019-02-02 15:31:18 -0600

    Update store

--- src/services/store.js
+++ src/services/store.js
@@ -3,11 +3,13 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import userReducer from './user/reducer';
+import itemReducer from './item/reducer';
 import uiReducer from './UI/reducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   ui: uiReducer,
+  item: itemReducer,
 });
 
 let middleware = applyMiddleware(thunk, logger);
